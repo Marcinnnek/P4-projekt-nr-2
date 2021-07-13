299c1e21abf08074621617d0574eb588a5f60952
Marcin <mzuziak2@gmail.com>
2021-07-13 21:46:58 +0200

    test

--- DBservice/myDB.cs
+++ DBservice/myDB.cs
@@ -13,7 +13,7 @@ namespace P4_projekt_nr_2.DBservice
     public class myDB
     {
         private readonly IDbConnection myDBconnection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbBAN"].ConnectionString);
-        
+
         public IEnumerable<Facility> GetFacilities()
         {
             using (myDBconnection)
@@ -21,6 +21,59 @@ namespace P4_projekt_nr_2.DBservice
                 if (myDBconnection.State == ConnectionState.Closed)
                     myDBconnection.Open();
                 return myDBconnection.Query<Facility>("SELECT * FROM BAN.Facility").ToList();
+            }
+        }
+
+        public bool InsterFacility(Facility ObiektB)
+        {
+            using (IDbConnection dbConnection = myDBconnection)
+            {
+                if (myDBconnection.State == ConnectionState.Closed)
+                    myDBconnection.Open();
+                var result = myDBconnection.Execute("INSERT INTO BAN.Facility (Facility_Name, FacilityDescription, SAP) VALUES ( @Name, @DESCRIPTION, @SAP)",
+                    new
+                    {
+                        Name = ObiektB.Facility_Name,
+                        DESCRIPTION = ObiektB.FacilityDescription,
+                        SAP = ObiektB.SAP
+                    });
+
+                return result == 1;
+            }
+        }
+
+        public bool UpdateFacility(Facility ObiektB)
+        {
+            using (IDbConnection dbConnection = myDBconnection)
+            {
+                if (myDBconnection.State == ConnectionState.Closed)
+                    myDBconnection.Open();
+                var result = myDBconnection.Execute("UPDATE BAN.Facility SET Facility_Name = @Name , FacilityDescription = @Description, SAP = @Sap WHERE ID_Facility = @IDf",
+                    new
+                    {   
+                        IDf = ObiektB.ID_Facility,
+                        Name = ObiektB.Facility_Name,
+                        DESCRIPTION = ObiektB.FacilityDescription,
+                        SAP = ObiektB.SAP
+                    });
+
+                return result == 1;
+            }
+        }
+
+        public bool DeleteFacility(int ObiektIDf)
+        {
+            using (IDbConnection dbConnection = myDBconnection)
+            {
+                if (myDBconnection.State == ConnectionState.Closed)
+                    myDBconnection.Open();
+                var result = myDBconnection.Execute(@"DELETE FROM BAN.Facility WHERE ID_Facility = @IDf)",
+                    new
+                    {
+                        IDf = ObiektIDf
+                    });
+
+                return result == 1;
             }
         }
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,15 +21,55 @@ namespace P4_projekt_nr_2
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int ID_Facility = 0;
+        private int FacilityID = 0;
+
 
         public MainWindow()
         {
             InitializeComponent();
-            myDB getObjects = new myDB();
-            DGFacilityList.DataContext = getObjects.GetFacilities();
+            RefreshDataBase();
+
+            IDCheckButtonEnabled();
 
         }
+
+        private void IDCheckButtonEnabled()
+        {
+            if (FacilityID == 0)
+            {
+                btDeleteContent.IsEnabled = false;
+                btUpdateContent.IsEnabled = false;
+                btSelectObject.IsEnabled = false;
+            }
+            else
+            {
+                btDeleteContent.IsEnabled = true;
+                btUpdateContent.IsEnabled = true;
+                btSelectObject.IsEnabled = true;
+            }
+        }
+
+        private void RefreshDataBase()
+        {
+            myDB getObjects = new myDB();
+            DGFacilityList.DataContext = getObjects.GetFacilities();
+        }
+
+        private void DGFacilityList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            IDCheckButtonEnabled();
+
+            Facility FC = DGFacilityList.SelectedItem as Facility;
+
+            FacilityID = FC.ID_Facility;
+            tbFacilityName.Text = FC.Facility_Name;
+            tbFacilityDescription.Text = FC.FacilityDescription;
+            tbSAP.Text = FC.SAP;
+            Console.WriteLine(FacilityID+"  "+ FC.Facility_Name +"  "+ FC.FacilityDescription+"  "+ FC.SAP);
+        }
+
+
+
 
         public void TB_GetFocus_FacilityName(object sender, RoutedEventArgs e)
         {
@@ -61,14 +101,47 @@ namespace P4_projekt_nr_2
             }
         }
 
-        private void tbFacilityDescription_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void Button_ClickInsertContent(object sender, RoutedEventArgs e)
         {
-            if (tbFacilityName.Text == "Podaj krótki opis obiektu!")
+            IDCheckButtonEnabled();
+            Facility myNUDFacility = new Facility()
             {
-                TextBox tbFacilityDescription = (TextBox)sender;
-                tbFacilityDescription.Text = string.Empty;
-                tbFacilityDescription.GotFocus -= TB_GetFocus_FacilityDescription;
-            }
+                ID_Facility = FacilityID,
+                Facility_Name = tbFacilityName.Text,
+                FacilityDescription = tbFacilityDescription.Text,
+                SAP = tbSAP.Text
+            };
+
+            myDB getObjects = new myDB();
+            getObjects.InsterFacility(myNUDFacility);
+            MessageBox.Show("Dodano nowy obiekt!", "Obiekt budowlany");
+            RefreshDataBase();
+        }
+
+        private void Button_Click_DeleteContent(object sender, RoutedEventArgs e)
+        {
+            IDCheckButtonEnabled();
+            myDB getObjects = new myDB();
+            getObjects.DeleteFacility(FacilityID);
+            MessageBox.Show("Usunięto obiekt!", "Obiekt budowlany");
+            RefreshDataBase();
+        }
+
+        private void btUpdateContent_Click(object sender, RoutedEventArgs e)
+        {
+            IDCheckButtonEnabled();
+            Facility myNUDFacility = new Facility()
+            {
+                ID_Facility = FacilityID,
+                Facility_Name = tbFacilityName.Text,
+                FacilityDescription = tbFacilityDescription.Text,
+                SAP = tbSAP.Text
+            };
+
+            myDB getObjects = new myDB();
+            getObjects.UpdateFacility(myNUDFacility);
+            MessageBox.Show("Uaktualniono obiekt!", "Obiekt budowlany");
+            RefreshDataBase();
         }
     }
 }
